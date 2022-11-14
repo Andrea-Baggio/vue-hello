212b23ef9453962491cb06d3e1a791ce5f2c95d1
Andrea-Baggio <andreabaggio800@gmail.com>
2022-11-14 09:33:59 +0100

    piccole modifiche

--- js/script.js
+++ js/script.js
@@ -1,16 +1,7 @@
-/*
-Descrizione:
-Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
-Bonus:
-Aggiungere alla pagina un’immagine, presa anch’essa da un data.
-*/
-
 let app = new Vue({
-  el: '#title',
+  el: 'header',
   data: {
     message: 'Gatos!',
-  
-
-  linkPic: 'https://wips.plug.it/cips/paginegialle.it/magazine/cms/2022/07/curiosita-gatto.jpg?w=744&h=418&a=c',
-}
+    linkPic: 'https://wips.plug.it/cips/paginegialle.it/magazine/cms/2022/07/curiosita-gatto.jpg?w=744&h=418&a=c',
+  }
 })
